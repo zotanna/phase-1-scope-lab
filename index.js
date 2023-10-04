e02d86a64feed73aeaeebd537b0d58ad0f9ae187
denis <denniiskim@gmail.com>
2023-10-04 05:44:08 +0300

    My assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'John';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Bob'; 
+}
+function upperCaseCustomerName() {
+    if (typeof customerName === 'string') {
+        customerName = customerName.toUpperCase();
+      } else {
+        console.log("customerName is not a string.");
+      }
+    }
+    function setBestCustomer() {
+        bestCustomer = 'not bob';
+      }
+      var bestCustomer = 'not bob';
+
+function overwriteBestCustomer() {
+  bestCustomer = 'new customer';
+}
